4e018a138a8a3a01d026cab29080a9b9d274de44
Serhii Myshko <sergeiown@gmail.com>
2023-04-12 15:57:22 +0300

    add .some and. every methods

---
+++ every.js
@@ -0,0 +1,17 @@
+/* метод проходиться по кожному елементу масиву і викликає функцію callback 
+для кожного елемента. Якщо хоча б одна ітерація поверне false, то метод 
+поверне false. Якщо ж усі ітерації повернуть true, то метод поверне true. */
+
+Array.prototype.myEvery = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (!callback(this[i], i, this)) {
+            return false;
+        }
+    }
+    return true;
+};
+
+const arr = [2, 2, 4, 4, 6];
+const areEven = arr.myEvery((num) => num % 2 === 0);
+
+console.log(areEven);
---
+++ some.js
@@ -0,0 +1,18 @@
+/* функція mySome проходить усіма елементами масиву і викликає callback 
+функцію для кожного елемента. Якщо хоча б один елемент задовольняє умову, 
+повернути true, в іншому випадку повернути false.
+В усьому подібний до метода .every */
+
+Array.prototype.mySome = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+        if (callback(this[i], i, this)) {
+            return true;
+        }
+    }
+    return false;
+};
+
+const arr = [1, 2, 3, 4, 5];
+const hasEven = arr.mySome((num) => num % 2 === 0);
+
+console.log(hasEven);
